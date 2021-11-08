8a3ecada02e5c1ae64cb24a7ea62cf0d9b07df36
No767 <winterpiece104@gmail.com>
2021-11-07 18:37:34 -0800

    Attempt to start site

--- site/Rin-Site/src/components/HelloWorld.vue
+++ site/Rin-Site/src/components/HelloWorld.vue
@@ -26,11 +26,6 @@ const count = ref(0)
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
 </template>
 
 <style scoped>
