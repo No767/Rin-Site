844e850331c112f2110500c3beddeca90f686461
No767 <73260931+No767@users.noreply.github.com>
2022-03-08 20:52:51 -0800

    format files

--- Rin-Site-React/src/main.jsx
+++ Rin-Site-React/src/main.jsx
@@ -2,19 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Commands from "./routes/commands"
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from "react-router-dom";
+import Commands from "./routes/commands";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="commands" element={<Commands />} />
-        </Routes>
-    </BrowserRouter>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="commands" element={<Commands />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById("root")
 );
