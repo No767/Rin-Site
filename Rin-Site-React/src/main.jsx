5b5d927c69f1281786745931196f4f454a1b395b
No767 <73260931+No767@users.noreply.github.com>
2022-03-08 20:51:55 -0800

    add testing page for commands
    still wip

--- Rin-Site-React/src/main.jsx
+++ Rin-Site-React/src/main.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Commands from "./routes/commands"
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="commands" element={<Commands />} />
+        </Routes>
+    </BrowserRouter>,
   document.getElementById("root")
 );
---
+++ Rin-Site-React/src/routes/commands.jsx
@@ -0,0 +1,48 @@
+import Stack from '@mui/material/Stack';
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import PhoneIcon from '@mui/icons-material/Phone';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
+import PhoneMissedIcon from '@mui/icons-material/PhoneMissed';
+import {createTheme} from "@mui/material/styles";
+
+export default function Commands() {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    const theme = createTheme({
+        components: {
+            MuiButton: {
+                styleOverrides: {
+                    root: {
+                        fontFamily: "Quicksand",
+                    }
+                },
+            },
+        },
+        palette: {
+            primary: {
+                main: "#ffffff",
+            },
+        },
+    });
+    return (
+        <body>
+        <Stack spacing={2} pt={10} className="items-center">
+            <h1 className="text-white">Commands</h1>
+
+            <Tabs
+                indicatorColor="secondary"
+                aria-label="icon position tabs example"
+                textColor="secondary"
+            >
+                <Tab icon={<PhoneIcon />} label="top" />
+            </Tabs>
+        </Stack>
+        </body>
+    );
+}
